d29cedf3e5f4aaaf7d31e2485c4662cb9d3f7240
Vitaly Kopytich <kopytich.vitaly@gmail.com>
2016-11-24 17:10:53 +0300

    Simple neural network on JavaScript

---
+++ train.js
@@ -0,0 +1,19 @@
+const brain = require('brain.js');
+var net = new brain.NeuralNetwork();
+const fs = require('fs');
+const mnist = require('mnist');
+const set = mnist.set(1000, 0);
+const trainingSet = set.training;
+net.train(trainingSet, {
+    errorThresh: 0.005,
+    iterations: 20000,
+    log: true,
+    logPeriod: 1,
+    learningRate: 0.3
+});
+
+let wstream = fs.createWriteStream('./data/mnistTrain.json');
+wstream.write(JSON.stringify(net.toJSON(), null, 2));
+wstream.end();
+
+console.log('MNIST dataset with Brain.js train done.');
